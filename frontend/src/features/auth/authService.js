f7f34b225d1cd184d03cb6542d7f6803be7d0966
hanzala434 <hanzalay959@gmail.com>
2024-11-10 16:12:29 +0500

    url

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const API_URL='http://localhost:8000/api/users'
+const API_URL='https://event-ease-rho.vercel.app/api/users'
 
 //register user
 const register=async(userData)=>{
