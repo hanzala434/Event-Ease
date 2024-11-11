a6cec6b762ba65e9f2eaba8b46453cfc7b00a5f9
hanzala434 <hanzalay959@gmail.com>
2024-11-12 00:28:56 +0500

    youtube

--- backend/api/index.js
+++ backend/api/index.js
@@ -4,8 +4,8 @@ const colors=require('colors')
 const cors = require('cors');
 const dotenv=require('dotenv').config()
 const port = process.env.PORT || 8000;
-const connectDB=require('./config/db')
-const {errorHandler}=require('./middleware/errorMiddleware')
+const connectDB=require('../config/db')
+const {errorHandler}=require('../middleware/errorMiddleware')
 const { injectSpeedInsights }=require('@vercel/speed-insights');
  
 injectSpeedInsights();
@@ -30,21 +30,21 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
-app.use('/api/vendors',require('./Routes/VendorsRoutes'));
-app.use('/api/users',require('./Routes/UserRoutes'));
-app.use('/api/setup-budget',require('./Routes/BudgetRoutes'));
-app.use('/api/services',require('./Routes/ServicesRoutes'));
-app.use('/api/booking',require('./Routes/BookingRoutes'));
-app.use('/api/packages',require('./Routes/PackagesRoutes'));
+app.use('/api/vendors',require('../Routes/VendorsRoutes'));
+app.use('/api/users',require('../Routes/UserRoutes'));
+app.use('/api/setup-budget',require('../Routes/BudgetRoutes'));
+app.use('/api/services',require('../Routes/ServicesRoutes'));
+app.use('/api/booking',require('../Routes/BookingRoutes'));
+app.use('/api/packages',require('../Routes/PackagesRoutes'));
 
 
 // Serve frontend
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../frontend/build')));
+    app.use(express.static(path.join(__dirname, '.../frontend/build')));
   
     app.get('*', (req, res) =>
       res.sendFile(
-        path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+        path.resolve(__dirname, '.../', 'frontend', 'build', 'index.html')
       )
     );
   } else {
